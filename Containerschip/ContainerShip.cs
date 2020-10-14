b95c4285ae57dc544c36ba8c7a1ae2ef5d88f647
Robbert Lengton <robbert.lengton@hotmail.nl>
2020-10-14 13:11:46 +0200

    Classes toegevoegd

---
+++ Containerschip/ContainerShip.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Containerschip
+{
+    public class ContainerShip
+    {
+        /* For example:
+         * - Calculate maxWeight (900): length (3) x width (2) x maxContainerSpaceWeight (150)
+         * - requiredWeight      (450): maxWeight (900) / 2 
+         */
+        private readonly int maxContainerSpaceWeight = 150;
+        private int maxWeight;
+        private int requiredWeight;
+    }
+}
---
+++ Containerschip/Enums.cs
@@ -0,0 +1,10 @@
+﻿namespace Containerschip
+{
+    public enum ContainerTypes
+    {
+        Normal = 0,
+        Coolable = 1,
+        CoolableValuable = 2,
+        Valuable = 3
+    }
+}
